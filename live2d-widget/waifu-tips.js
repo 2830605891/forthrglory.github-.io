c7ed895b0c904091e5ce782d891ee086552a6310
赵家巍 <forthrglory@Forthrglory.local>
2020-10-23 09:33:34 +0800

    Site updated: 2020-10-23 09:33:34

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -19,7 +19,7 @@ function loadWidget(config) {
 	sessionStorage.removeItem("waifu-text");
 	document.body.insertAdjacentHTML("beforeend", `<div id="waifu">
 			<div id="waifu-tips"></div>
-			<canvas id="live2d" width="500" height="500"></canvas>
+			<canvas id="live2d" width="100" height="100"></canvas>
 			<div id="waifu-tool">
 				<span class="fa fa-lg fa-comment"></span>
 				<span class="fa fa-lg fa-paper-plane"></span>
